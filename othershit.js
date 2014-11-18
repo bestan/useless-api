5ff1e8a51beb222a824caec3ddaa47e127ebb24d
Tariq Patel <tariq.patel@gmail.com>
2014-11-18 11:59:33 +0000

    YO

--- othershit.js
+++ othershit.js
@@ -8,4 +8,11 @@ module.exports.init = function(app){
   app.post('/echo', function(req,res){
     res.status(200).send(req.body);
   });
+
+  app.get('/yo', function(req, res){
+    if (req.query.name !== undefined){
+      return res.status(200).send('Yo, ' + req.query.name + '!');
+    }
+    return res.status(200).send('Yo');
+  });
 };
